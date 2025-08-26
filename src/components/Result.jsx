faf4c4d7df400f4754713bb43fbf34ada1121dc0
Jeremy Estes <estesjeremy96@gmail.com>
2025-08-26 11:28:03 -0400

    test deployment commit

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,13 +4,14 @@ function Result({result, onReset}) {
   if (!result) return <Rings />;
   return (
     <div className="lg:col-2 w-full max-w-100 my-8 justify-self-center">
+
       <div className="rounded-2xl border-lime border-1 bg-dark mb-5 text-left">
         <h2 className='text-dark font-body px-4 py-2  uppercase text-3xl bg-lime rounded-t-2xl'>{result.zodiac}</h2>
         <img src={result.image} alt="" />
         <div className='px-4 pt-2 pb-5'>
           <h3 className='text-lime font-body uppercase'>{result.range}</h3>
           <h4 className="text-3xl">{result.name}</h4>
-          <p className="">{result.quote}</p>
+          <p className="test">{result.quote}</p>
         </div>
       </div>
       
