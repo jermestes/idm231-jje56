0347525edd4a29f0720c0efa8d9f633053347c0a
Jeremy Estes <estesjeremy96@gmail.com>
2025-08-29 12:36:40 -0400

    fix safari player reveal centering bug

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ import Rings from './Rings.jsx';
 function Result({result, onReset}) {
   if (!result) return <Rings />;
   return (
-    <div className="lg:col-2 w-full max-w-100 my-8 justify-self-center">
+    <div className="lg:col-2 w-full max-w-100 my-8 justify-self-center m-auto">
 
       <div className="rounded-2xl border-lime border-1 bg-dark mb-5 text-left">
         <h2 className='text-dark font-body px-4 py-2  uppercase text-3xl bg-lime rounded-t-2xl'>{result.zodiac}</h2>
